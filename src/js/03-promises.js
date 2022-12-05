20800ec8980eddeba1d5f380a873c44c3219d9a5
Olha-Suchok <suchok_olya@ukr.net>
2022-12-05 10:46:53 +0200

    The third  task is completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget;
+  let position = 0;
+  let amountValue = Number.parseInt(amount.value);
+  let delayValue = Number.parseInt(delay.value);
+  let stepValue = Number.parseInt(step.value);
+
+  intervalId = setInterval(() => {
+    position += 1;
+    delayValue += stepValue;
+    amountValue -= 1;
+
+    createPromise(position, delayValue)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    if (amountValue === 0) {
+      clearInterval(intervalId);
+    }
+  }, delayValue);
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
